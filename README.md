eaad5d4cb6cb3d1c1ff76d2fd8871c0fad0b3759
Eric Wiener <ericw1188@gmail.com>
2019-02-02 12:57:59 -0500

    fixing errors committing too large files

--- README.md
+++
@@ -1,3 +0,0 @@
-`oyez-crawler` crawls [oyez](https://www.oyez.org) to download transcripts of supreme court cases.
-
-More detailed README coming soon, but usage can be inferred from `driver.js`
--- driver.js
+++
@@ -1,9 +0,0 @@
-var crawler = require('./crawler.js')
-var writeFile = require('write');
-
-async function crawl(url){
-  await crawler.scrapeOyez();
-  // writeFile.sync("output/transcripts.js", JSON.stringify(results));
-}
-
-crawl("https://www.oyez.org/cases/2018");
